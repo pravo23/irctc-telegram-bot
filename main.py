ee3a116a15e1db018919ed1504388759d80f8279
pravo23 <pravo23@192.168.1.5>
2024-03-18 01:23:19 +0530

    pylint of the code using autopep8

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import rail_query_helper
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = TeleBot(BOT_TOKEN)
 
+
 @bot.message_handler(commands=["start", "help"])
 def greet(message):
     greet_message = """
@@ -20,37 +21,53 @@ def greet(message):
     """
     bot.send_message(message.chat.id, greet_message, parse_mode='Markdown')
 
+
 @bot.message_handler(commands=["pnr"])
 def pnr_status(message):
-    input_message="Please Enter the PNR number!"
-    sent_msg = bot.send_message(message.chat.id, input_message, parse_mode="Markdown")
+    input_message = "Please Enter the PNR number!"
+    sent_msg = bot.send_message(
+        message.chat.id,
+        input_message,
+        parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, pnr_status_handler)
+
 
 def pnr_status_handler(message):
     pnr_number = message.text
     status_msg = rail_query_helper.pnr_status_message(pnr_number)
     bot.send_message(message.chat.id, status_msg, parse_mode="Markdown")
 
+
 @bot.message_handler(commands=["searchStation"])
 def get_station(message):
-    input_message="Please enter the station code!"
-    sent_message = bot.send_message(message.chat.id, input_message, parse_mode="Markdown")
+    input_message = "Please enter the station code!"
+    sent_message = bot.send_message(
+        message.chat.id,
+        input_message,
+        parse_mode="Markdown")
     bot.register_next_step_handler(sent_message, get_station_handler)
+
 
 def get_station_handler(message):
     station_code = message.text
     resp_message = rail_query_helper.search_station_response(station_code)
     bot.send_message(message.chat.id, resp_message, parse_mode="Markdown")
 
+
 @bot.message_handler(commands=["searchTrain"])
 def get_train(message):
-    input_message="Please enter the train number!"
-    sent_message = bot.send_message(message.chat.id, input_message, parse_mode="Markdown")
+    input_message = "Please enter the train number!"
+    sent_message = bot.send_message(
+        message.chat.id,
+        input_message,
+        parse_mode="Markdown")
     bot.register_next_step_handler(sent_message, get_train_handler)
+
 
 def get_train_handler(message):
     train_code = message.text
     resp_message = rail_query_helper.search_train_response(train_code.strip())
     bot.send_message(message.chat.id, resp_message, parse_mode="Markdown")
+
 
 bot.infinity_polling()
--- rail_query_helper.py
+++ rail_query_helper.py
@@ -2,7 +2,7 @@ import os
 import requests
 from constants import railApiEndpoints
 
-def get_rail_query_info(url : str):
+def get_rail_query_info(url: str):
     HOST = os.environ.get("HOST")
     API_KEY = os.environ.get("API_KEY")
     url = url.format(HOST)
@@ -14,39 +14,51 @@ def get_rail_query_info(url : str):
     response = requests.get(url, headers=headers)
     return response.json()
 
+
 def fetch_pnr_status(pnr: str):
     fetch_pnr_status_endpoint = railApiEndpoints['pnrStatus'].format(pnr)
     url = f"https://{{}}{fetch_pnr_status_endpoint}"
     return get_rail_query_info(url)
 
+
 def search_station(station_code):
-    search_station_endpoint = railApiEndpoints['searchStation'].format(station_code)
+    search_station_endpoint = railApiEndpoints['searchStation'].format(
+        station_code)
     url = f"https://{{}}{search_station_endpoint}"
     return get_rail_query_info(url)
+
 
 def search_train(train_code):
     search_train_endpoint = railApiEndpoints['searchTrain'].format(train_code)
     url = f"https://{{}}{search_train_endpoint}"
     return get_rail_query_info(url)
 
+
 def train_between_stations(from_station_code, to_station_code, date):
-    train_between_stations_endpoint = railApiEndpoints['trainBetweenStations'].format(from_station_code, to_station_code, date)
+    train_between_stations_endpoint = railApiEndpoints['trainBetweenStations'].format(
+        from_station_code, to_station_code, date)
     url = f"https://{{}}{train_between_stations_endpoint}"
     return get_rail_query_info(url)
 
+
 def live_train_status(train_code, start_day):
-    live_train_status_endpoint = railApiEndpoints['liveTrainStatus'].format(train_code, start_day)
+    live_train_status_endpoint = railApiEndpoints['liveTrainStatus'].format(
+        train_code, start_day)
     url = f"https://{{}}{live_train_status_endpoint}"
     return get_rail_query_info(url)
 
+
 def train_schedule(train_code):
-    train_schedule_endpoint = railApiEndpoints['trainSchedule'].format(train_code)
+    train_schedule_endpoint = railApiEndpoints['trainSchedule'].format(
+        train_code)
     url = f"https://{{}}{train_schedule_endpoint}"
     return get_rail_query_info(url)
 
+
 def seat_availability(params):
     class_type, from_station_code, quota, to_station_code, train_code, date = (
-        params.get(key) for key in ['class_type', 'from_station_code', 'quota', 'to_station_code', 'train_code', 'date'])
+        params.get(key) for key in [
+            'class_type', 'from_station_code', 'quota', 'to_station_code', 'train_code', 'date'])
 
     seat_availability_endpoint = railApiEndpoints['seatAvailability'].format(
         class_type, from_station_code, quota, to_station_code, train_code, date
@@ -54,27 +66,36 @@ def seat_availability(params):
     url = f"https://{{}}{seat_availability_endpoint}"
     return get_rail_query_info(url)
 
+
 def train_classes(train_code):
-    train_classes_endpoint = railApiEndpoints['trainClasses'].format(train_code)
+    train_classes_endpoint = railApiEndpoints['trainClasses'].format(
+        train_code)
     url = f"https://{{}}{train_classes_endpoint}"
     return get_rail_query_info(url)
 
+
 def fetch_fair(train_code, from_station_code, to_station_code):
-    fetch_fair_endpoint = railApiEndpoints['fetchFair'].format(train_code, from_station_code, to_station_code)
+    fetch_fair_endpoint = railApiEndpoints['fetchFair'].format(
+        train_code, from_station_code, to_station_code)
     url = f"https://{{}}{fetch_fair_endpoint}"
     return get_rail_query_info(url)
 
+
 def train_by_station(station_code):
-    train_by_station_endpoint = railApiEndpoints['trainByStations'].format(station_code)
+    train_by_station_endpoint = railApiEndpoints['trainByStations'].format(
+        station_code)
     url = f"https://{{}}{train_by_station_endpoint}"
     return get_rail_query_info(url)
 
+
 def live_stations(from_station_code, to_station_code, hours):
-    live_stations_endpoint = railApiEndpoints['liveStations'].format(from_station_code, to_station_code, hours)
+    live_stations_endpoint = railApiEndpoints['liveStations'].format(
+        from_station_code, to_station_code, hours)
     url = f"https://{{}}{live_stations_endpoint}"
     return get_rail_query_info(url)
 
-## methods for the formatted response
+# methods for the formatted response
+
 
 def pnr_status_message(pnr: str):
 
@@ -85,8 +106,9 @@ def pnr_status_message(pnr: str):
 
     return "Unable to fetch the pnr : *{pnr}*"
 
-## TODO: fetch the code instead, based on search query?
-def search_station_response(station_code : str):
+# TODO: fetch the code instead, based on search query?
+
+def search_station_response(station_code: str):
     resp = search_station(station_code)
     if resp.get('status') and resp.get('message') == 'Success':
         for station in resp.get('data'):
@@ -95,6 +117,7 @@ def search_station_response(station_code : str):
         return f"No station available with station code: *{station_code}*"
 
     return f"Could not fetch the data. Try again!"
+
 
 def search_train_response(train_code: str):
     resp = search_train(train_code)
