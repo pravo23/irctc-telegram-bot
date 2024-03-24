3d6eef9ffff375d464e87c4d59a18de9a51b5327
pravo23 <pravo23@192.168.1.6>
2024-03-24 19:58:31 +0530

    updated the method documentation

--- rail_query_helper.py
+++ rail_query_helper.py
@@ -2,8 +2,16 @@ import os
 import requests
 from constants import railApiEndpoints
 
+def get_rail_query_info(url: str) -> dict:
+    """
+    Sends an HTTP GET request to the specified URL and returns the JSON response.
 
-def get_rail_query_info(url: str):
+    Args:
+        url (str): The URL to which the GET request is sent.
+
+    Returns:
+        dict: A dictionary containing the JSON response from the API.
+    """
     HOST = os.environ.get("HOST")
     API_KEY = os.environ.get("API_KEY")
     url = url.format(HOST)
@@ -15,48 +23,115 @@ def get_rail_query_info(url: str):
     response = requests.get(url, headers=headers)
     return response.json()
 
+def fetch_pnr_status(pnr: str) -> dict:
+    """
+    Fetches the PNR (Passenger Name Record) status from the railway API for the given PNR number.
 
-def fetch_pnr_status(pnr: str):
+    Args:
+        pnr (str): The PNR number for which the status is requested.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including PNR status information.
+    """
     fetch_pnr_status_endpoint = railApiEndpoints['pnrStatus'].format(pnr)
     url = f"https://{{}}{fetch_pnr_status_endpoint}"
     return get_rail_query_info(url)
 
+def search_station(station_code: str) -> dict:
+    """
+    Searches for a railway station by its station code using the railway API.
 
-def search_station(station_code):
+    Args:
+        station_code (str): The station code to search for.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including station information.
+    """
     search_station_endpoint = railApiEndpoints['searchStation'].format(
         station_code)
     url = f"https://{{}}{search_station_endpoint}"
     return get_rail_query_info(url)
 
+def search_train(train_code: str) -> dict:
+    """
+    Searches for a train by its train code using the railway API.
 
-def search_train(train_code):
+    Args:
+        train_code (str): The train code to search for.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including train information.
+    """
     search_train_endpoint = railApiEndpoints['searchTrain'].format(train_code)
     url = f"https://{{}}{search_train_endpoint}"
     return get_rail_query_info(url)
 
+def train_between_stations(
+        from_station_code: str,
+        to_station_code: str,
+        date: str) -> dict:
+    """
+    Fetches the list of trains running between two stations on a specified date using the railway API.
 
-def train_between_stations(from_station_code, to_station_code, date):
+    Args:
+        from_station_code (str): The station code of the starting station.
+        to_station_code (str): The station code of the destination station.
+        date (str): The date of travel in YYYYMMDD format.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including train schedule information.
+    """
     train_between_stations_endpoint = railApiEndpoints['trainBetweenStations'].format(
         from_station_code, to_station_code, date)
     url = f"https://{{}}{train_between_stations_endpoint}"
     return get_rail_query_info(url)
 
+def live_train_status(train_code: str, start_day: int) -> dict:
+    """
+    Fetches the live status of a train using the railway API.
 
-def live_train_status(train_code, start_day):
+    Args:
+        train_code (str): The train code for which the live status is requested.
+        start_day (int): The start day from today (0 for today, 1 for tomorrow, and so on).
+
+    Returns:
+        dict: A dictionary containing the response from the API, including live train status information.
+    """
     live_train_status_endpoint = railApiEndpoints['liveTrainStatus'].format(
         train_code, start_day)
     url = f"https://{{}}{live_train_status_endpoint}"
     return get_rail_query_info(url)
 
+def train_schedule(train_code: str) -> dict:
+    """
+    Fetches the schedule of a train using the railway API.
 
-def train_schedule(train_code):
+    Args:
+        train_code (str): The train code for which the schedule is requested.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including train schedule information.
+    """
     train_schedule_endpoint = railApiEndpoints['trainSchedule'].format(
         train_code)
     url = f"https://{{}}{train_schedule_endpoint}"
     return get_rail_query_info(url)
 
+def fetch_fair(
+        train_code: str,
+        from_station_code: str,
+        to_station_code: str) -> dict:
+    """
+    Fetches the fare details for a train between two stations using the railway API.
 
-def fetch_fair(train_code, from_station_code, to_station_code):
+    Args:
+        train_code (str): The train code.
+        from_station_code (str): The station code of the starting station.
+        to_station_code (str): The station code of the destination station.
+
+    Returns:
+        dict: A dictionary containing the response from the API, including fare details.
+    """
     fetch_fair_endpoint = railApiEndpoints['fetchFair'].format(
         train_code, from_station_code, to_station_code)
     url = f"https://{{}}{fetch_fair_endpoint}"
